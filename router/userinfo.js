98ef34bead5ea6d795a6452656fa2f30160b04ce
lcruan <1416802331@qq.com>
2024-02-01 21:23:57 +0800

    更新用户信息的路由和处理函数

--- router/userinfo.js
+++ router/userinfo.js
@@ -8,4 +8,7 @@ const userinfo_handler = require('../router_handler/userinfo')
 // 获取用户基本信息的路由
 router.get('/userinfo', userinfo_handler.getUserInfo)
 
+// 更新用户信息的路由
+router.post('/userinfo', userinfo_handler.updateUserInfo)
+
 module.exports = router
--- router_handler/userinfo.js
+++ router_handler/userinfo.js
@@ -21,3 +21,8 @@ exports.getUserInfo = (req, res) => {
         })
     })
 }
+
+// 更新用户基本信息处理函数
+exports.updateUserInfo = (req, res) => {
+    res.send('ok')
+}
