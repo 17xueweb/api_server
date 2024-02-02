eab1b7e2cb33f6ee20ea39bb5d83c4a5cef0e170
lcruan <1416802331@qq.com>
2024-02-02 23:07:14 +0800

    校验客户端输入新增文章分类的字段

--- router/artcate.js
+++ router/artcate.js
@@ -1,6 +1,5 @@
 // 这是文章分类的路由模块
 
-
 const express = require('express')
 
 const router = express.Router()
@@ -8,11 +7,16 @@ const router = express.Router()
 // 导入文章分类的路由处理函数
 const artCate_handler = require('../router_handler/artcate')
 
+// 导入验证数据的中间件
+const expressJoi = require('@escook/express-joi')
+// 导入需要的验证规则对象
+const { add_cate_schema } = require('../schema/artcate')
+
 // 获取文章分类列表数据的路由
 router.get('/cates', artCate_handler.getArtCates)
 
 // 新增文章分类的路由
-router.post('/addcates', artCate_handler.addArticleCates)
+router.post('/addcates', expressJoi(add_cate_schema), artCate_handler.addArticleCates)
 
 module.exports = router
 
